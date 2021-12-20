092073856249d778e05915739e8c372dbd4a1e57
Benjamin Zagorsky <iamzags@gmail.com>
2021-12-20 16:59:19 -0500

    removes extra lines

--- {{cookiecutter.project_slug}}/.platform/confighooks/predeploy/predeploy.sh
+++ {{cookiecutter.project_slug}}/.platform/confighooks/predeploy/predeploy.sh
@@ -9,7 +9,6 @@ source $PYTHONPATH/activate
 {%- if cookiecutter.feature_annotations == "on" -%}
 # START_FEATURE django_react
 {%- endif %}
-npm install -G nwb
 npm install
 ./node_modules/.bin/nwb build --no-vendor
 {%- if cookiecutter.feature_annotations == "on" %}
