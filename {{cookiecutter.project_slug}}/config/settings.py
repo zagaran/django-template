3a32fd234320b1da095d60168ef228e745f1f365
Benjamin Zagorsky <zags@users.noreply.github.com>
2025-06-05 15:03:39 -0400

    adds middleware import

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -181,7 +181,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
-    "common.middleware.health_check.HealthCheckMiddleware",
+    "common.middleware.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
